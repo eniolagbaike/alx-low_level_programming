f4a05a7d767abd1d9ceb566a4615374bd30710ac
Olugbaike Eniola Esther <eniolagbaike@gmail.com>
2023-02-20 10:08:28 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,13 +5,13 @@
  * Description: Write a program that prints all possible
  * different combinations of two digits
  * Numbers must be separated by ,, followed by a space
- * The two digits must be different
- * 01 and 10  are considered the same combination of the two digits 0 and 1
- * Print only the smallest combination of two digits
- * Numbers should be printed in ascending order, with two digits
+ * The three digits must be different
+ * 01 and 10 are considered the same combination of the three digits 0 and 1
+ * Print only the smallest combination of three digits
+ * Numbers should be printed in ascending order, with three digits
  * You can only use the putchar function
  * (every other function (printf, puts, etc…) is forbidden)
- * You can only use putchar five times maximum in your code
+ * You can only use putchar six times maximum in your code
  * You are not allowed to use any variable of type char
  * All your code should be in the main function
  * Return: 0
