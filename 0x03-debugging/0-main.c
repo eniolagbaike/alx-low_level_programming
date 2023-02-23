7345daaac878d6a4575af73403a43e889be2d9b3
Olugbaike Eniola Esther <eniolagbaike@gmail.com>
2023-02-23 03:07:47 -0800

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,8 +4,6 @@
  * main - tests function that prints if integer is positivr or negative
  * Return: 0
  */
-
-
 int main(void)
 
 {
@@ -14,6 +12,5 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
-
 	return (0);
 }
