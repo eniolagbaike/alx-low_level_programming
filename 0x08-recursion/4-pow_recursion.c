2c193351a13d11ba230cf211d8f0dc99a8fa8f0d
Olugbaike Eniola Esther <eniolagbaike@gmail.com>
2023-03-07 04:40:13 -0800

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _pow_recursion - 
- * @x:
- * @y: 
+ * _pow_recursion - Returns the value of x raised to the power of y 
+ * @x: the value to multiply
+ * @y: the times to multiply the value
  *
- * Return: 
+ * Return: the value multiplied y times
  */
 int _pow_recursion(int x, int y)
 {
@@ -14,6 +14,5 @@ int _pow_recursion(int x, int y)
 
 	if (y == 0)
 		return (1);
-	
 	return (x * _pow_recursion(x, y - 1));
 }
